d24154c328837ad18f74a96a176ff89d9d7ea1dc
Ariel Cordes <corde171@morris.umn.edu>
2019-10-15 10:20:58 -0500

    Type out manually the basic skeletons copied from dateserver; actually implementing things to come.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,45 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+    public static final int portNum = 6103;
+
+    public static void main(String[] args) {
+        String server;
+        // Use 127.0.0.1 aka localhost
+        if (args.length == 0) {
+            server = "127.0.0.1";
+        }
+        else {
+            server = args[0];
+        }
+        try {
+            // connect to server
+            Socket socket = new Socket(server, portNum);
+
+            // get inputstream
+            InputStream inputStream = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            // print input
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            //close socket
+
+            socket.close();
+
+        }
+        catch (ConnectException ce) {
+            System.out.println("We were unable to connect to " + server);
+            System.out.println("You should make sure the server is running.");
+        }
+        catch (IOException ioe) {
+            System.out.println("We caught an unexpected exception.");
+            System.err.println(ioe);
+        }
+    }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,38 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+    public static final int portNum = 6103;
+
+    public static void main(String[] args) {
+        try {
+            // Start listening a la NSA on the selected port
+            ServerSocket socket = new ServerSocket(portNum);
+
+            // Run forever, common for both servers and me from my problems
+            while (true) {
+                // Wait until someone connects
+                Socket client = socket.accept();
+                System.out.println("Got a request!");
+
+                // construct a writer so we can write to the socket,
+                // thereby sending something back to the client.
+                PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+
+                // Send current date back
+
+                writer.println(new java.util.Date().toString());
+
+                // Close it politely like you should with sockets and doors
+                client.close();
+            }
+
+        }
+        catch (IOException ioe) {
+            System.out.println("We caught an unexpected exception! OWO");
+            System.err.println(ioe);
+        }
+        
+    }
 }
